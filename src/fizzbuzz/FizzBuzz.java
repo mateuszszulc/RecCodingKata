056a5880f30f2e0748c102e7fa65839e5015a7fe
Mateusz Szulc <mateusz.szulc@gmail.com>
2012-03-01 14:10:36 +0100

    Small refactoring - moved i == 0 condition to createFizzBuzzStringFromInteger method.

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -4,9 +4,7 @@ public class FizzBuzz {
 
     public String parse(int i) {
         if (i < 0) throw new IllegalArgumentException();
-        if (i == 0) {
-            return "0";
-        }
+        
         String valueToDisplay;
         valueToDisplay = createFizzBuzzStringFromInteger(i);
         return valueToDisplay.length() == 0 ? String.valueOf(i) : valueToDisplay;
@@ -14,12 +12,9 @@ public class FizzBuzz {
 
     private static String createFizzBuzzStringFromInteger(int i) {
         StringBuilder valueToDisplay = new StringBuilder();
-        if ((i % 3) == 0) {
-            valueToDisplay.append("Fizz");
-        }
-        if ((i % 5) == 0) {
-            valueToDisplay.append("Buzz");
-        }
+        if (i == 0) return "0" ;
+        valueToDisplay.append(((i % 3) == 0) ? "Fizz" : "" );
+        valueToDisplay.append(((i % 5) == 0) ? "Buzz" : "" );
         return valueToDisplay.toString();
     }
 }
